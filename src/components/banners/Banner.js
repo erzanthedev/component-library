167ebe7c85c9bff3cb44257a768a51e476a21dce
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-10-17 08:36:38 +1100

    feat: added styling to banner component

--- src/components/banners/Banner.js
+++ src/components/banners/Banner.js
@@ -1,11 +1,16 @@
+import checkCircle from "../../assets/checkCircle.png";
+
 const Banner = () => {
   return (
-    <div>
-      <h6 className="banner-title">Congratulations!</h6>
-      <p className="banner-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-        pariatur, ipsum similique veniam.
-      </p>
+    <div className="banner">
+      <img src={checkCircle} alt="check circle" />
+      <div className="banner-content">
+        <h6 className="banner-title">Congratulations!</h6>
+        <p className="banner-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          pariatur, ipsum similique veniam.
+        </p>
+      </div>
     </div>
   );
 };
