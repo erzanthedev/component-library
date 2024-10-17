c5b334869f32061bd00dfcee6f267e84678d008c
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-10-17 13:47:13 +1100

    feat: added icons object for conditional rendering

---
+++ src/components/badges/icons.js
@@ -0,0 +1,15 @@
+import {
+  FaCircleCheck,
+  FaCircleXmark,
+  FaTriangleExclamation,
+  FaCircleInfo,
+} from "react-icons/fa6";
+
+const icons = {
+  success: <FaCircleCheck />,
+  warning: <FaTriangleExclamation />,
+  error: <FaCircleXmark />,
+  neutral: <FaCircleInfo />,
+};
+
+export default icons;
