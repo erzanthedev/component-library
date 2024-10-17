09fe4c4039f87627b63b87bb863de52bcfa971dc
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-10-17 22:22:03 +1100

    feat: added styling for multi or single line

--- src/components/banners/Banner.js
+++ src/components/banners/Banner.js
@@ -1,12 +1,12 @@
-import icons from "../badges/icons";
+import icons from "../banners/icons";
 
 const Banner = () => {
   return (
-    <div className="banner banner-warning">
+    <div className="banner banner-error multi">
       <span className="banner-icon">{icons.warning}</span>
       <div className="banner-content">
-        <h6 className="banner-title">Congratulations!</h6>
-        <p className="banner-text ">
+        <h2 className="banner-title">Congratulations!</h2>
+        <p className="banner-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
           pariatur, ipsum similique veniam.
         </p>
