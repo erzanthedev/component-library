8608a9611a35e803916fb2e53eccfa8ef6782534
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-10-19 01:16:46 +1100

    fix: improved tags with Semantic elements and BEM naming conventions for class to allow easy read

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,22 +1,23 @@
 import uploadIcon from "./cloud-upload.svg";
+
 const Card = () => {
   return (
-    <div className="card">
+    <article className="card">
       <div className="card-wrapper">
         <div className="card-content">
-          <div className="brand-icon">
-            <img src={uploadIcon} alt="cloud upload " />
-          </div>
-          <div className="content">
-            <h2 className="content-title">Easy Deployment</h2>
-            <p className="content-description">
+          <figure className="card-icon">
+            <img src={uploadIcon} alt="cloud upload icon for easy deployment" />
+          </figure>
+          <div className="card-details">
+            <h2 className="card-title">Easy Deployment</h2>
+            <p className="card-description">
               Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et
               magna sit morbi lobortis.
             </p>
           </div>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
