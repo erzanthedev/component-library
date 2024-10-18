a25887adf6a15cbc1edef6ff2db17de39584b0ed
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-10-19 02:17:56 +1100

    feat: added styling to card component

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -3,17 +3,22 @@ import uploadIcon from "./cloud-upload.svg";
 const Card = () => {
   return (
     <article className="card">
-      <div className="card-wrapper">
-        <div className="card-content">
-          <figure className="card-icon">
-            <img src={uploadIcon} alt="cloud upload icon for easy deployment" />
-          </figure>
-          <div className="card-details">
-            <h2 className="card-title">Easy Deployment</h2>
-            <p className="card-description">
-              Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et
-              magna sit morbi lobortis.
-            </p>
+      <div className="card-container">
+        <div className="card-wrapper">
+          <div className="card-content">
+            <figure className="card-icon">
+              <img
+                src={uploadIcon}
+                alt="cloud upload icon for easy deployment"
+              />
+            </figure>
+            <div className="card-details">
+              <h2 className="card-title">Easy Deployment</h2>
+              <p className="card-description">
+                Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.
+                Et magna sit morbi lobortis.
+              </p>
+            </div>
           </div>
         </div>
       </div>
