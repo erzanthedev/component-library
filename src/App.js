ed6bd07222f5c39683acea2d87f769fa5a007bed
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-10-23 08:53:03 +1100

    feat: added TestimonialWithoutImage Component with basic structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Card from "./components/cards/index.js";
-import { FaCloudArrowUp } from "react-icons/fa6";
+import TestimonialWithoutImage from "./components/testimonials/TestimonialWithoutImage";
 
 function App() {
   return (
     <main>
-      <Card icon={<FaCloudArrowUp />} iconBgColor="indigo">
-        <Card.Title>Clean Development</Card.Title>
-        <Card.Description>
-          Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et
-          magna sit morbi Ac tincidunt sapien vehicula erat auctor pellentesque
-          rhoncus.
-        </Card.Description>
-      </Card>
+      <TestimonialWithoutImage />
     </main>
   );
 }
---
+++ src/components/testimonials/TestimonialWithoutImage.js
@@ -0,0 +1,22 @@
+const TestimonialWithoutImage = () => {
+  return (
+    <div className="testimonial-no-image">
+      <div className="testimonial-wrapper">
+        <div className="testimonial">
+          <div className="leading-content">
+            <img src="" className="logo" alt="empty logo" />
+            <p className="text">
+              “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              expedita voluptas culpa sapiente alias molestiae. Numquam corrupti
+              in laborum sed rerum et corporis.”
+            </p>
+          </div>
+          <div className="cite"></div>
+        </div>
+      </div>
+      {/* Dot Pattern */}
+    </div>
+  );
+};
+
+export default TestimonialWithoutImage;
